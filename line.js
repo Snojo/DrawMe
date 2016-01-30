45bc7d257b4a09fb78aacb252fdd5d6d443c127b
solvib13@ru.is <solvib13@ru.is>
2016-01-30 23:34:57 +0000

    Text area markup

--- line.js
+++ line.js
@@ -7,14 +7,22 @@ var Line = Shape.extend({
 	draw: function(canvas) {
 		canvas.strokeStyle = this.color;
 		canvas.lineWidth = this.linewidth;
+
 		canvas.beginPath();
 		canvas.moveTo(this.pos.x, this.pos.y);
 		canvas.lineTo(this.size.x, this.size.y);
 		canvas.stroke();
+		canvas.closePath();
+
 		this.base(canvas);
 	},
 
 	drawing:function(point) {
+		this.size.x = point.x;
+		this.size.y = point.y;
+	},
+
+	startDrawing:function(point) {
 		this.size.x = point.x;
 		this.size.y = point.y;
 	},
--- text.js
+++ text.js
@@ -8,29 +8,35 @@ var Text = Shape.extend({
 	},
 
 	draw: function(canvas) {
-		//canvas.font="20px Georgia";
-		//canvas.fillText(this.textstring, this.pos.x, this.pos.y);
-		container.appendChild(this.textarea);
-		    // Tmp canvas is always cleared up before drawing.
-		    canvas.clearRect(0, 0, canvas.width, canvas.height);
-		     
-		    var x = this.pos.x;
-		    var y = this.pos.y;
-		    var width = this.size.x;
-		    var height = this.size.y;
-		     
-		    this.textarea.style.left = x + 'px';
-		    this.textarea.style.top = y + 'px';
-		    this.textarea.style.width = width + 'px';
-		    this.textarea.style.height = height + 'px';
-		     
-		    this.textarea.style.display = 'block';
-		
+		canvas.font="20px Georgia";
+		canvas.fillText(this.textstring, this.pos.x, this.pos.y);
 	},
 
 	drawing:function(point) {
 		this.size.x = point.x - this.pos.x;
 		this.size.y = point.y - this.pos.y;
+	},
+
+	startDrawing:function(point) {
+		container.appendChild(this.textarea);
+		    // Tmp canvas is always cleared up before drawing.
+	     
+	    var x = this.pos.x;
+	    var y = this.pos.y;
+	    var width = this.size.x;
+	    var height = this.size.y;
+	     
+	    this.textarea.style.left = point.x + 'px';
+	    this.textarea.style.top = point.y + 'px';
+	    this.textarea.style.width = 50 + 'px';
+	    this.textarea.style.height = 25 + 'px';
+	     
+	    this.textarea.style.display = 'block';
+	},
+
+	stopDrawing:function(point) {
+		this.textstring = this.textarea.value;
+		container.removeChild(this.textarea);
 	},
 
 	added: function(canvas) {
